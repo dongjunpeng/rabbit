77e817fce19ae00b3bf62ef1c0857f8f621f4318
zhenzong <zhenzong.peng@icloud.com>
2015-12-13 10:43:11 +0800

    使用spring boot

---
+++ src/main/java/com/buterfleoge/rabbit/Main.java
@@ -0,0 +1,32 @@
+package com.buterfleoge.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * 
+ * rabbit系统的入口
+ * 
+ * @author xiezhenzong
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.buterfleoge")
+@EntityScan(basePackages = "com.buterfleoge.whale.eo")
+@EnableJpaRepositories("com.buterfleoge.whale.dao")
+@EnableWebMvc
+@ImportResource("applicationContext.xml")
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
