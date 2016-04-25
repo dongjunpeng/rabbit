2b9b24bd209e4b7b98a161edb8ed8a3242337b6e
dongjunpeng <dongjunpeng0918@gmail.com>
2016-04-25 14:18:11 +0800

    travelRoute查询

--- src/main/java/com/buterfleoge/rabbit/controller/InformationController.java
+++
@@ -1,7 +0,0 @@
-package com.buterfleoge.rabbit.controller;
-
-public class InformationController {
-
-
-
-}
---
+++ src/main/java/com/buterfleoge/rabbit/controller/TravelController.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.buterfleoge.rabbit.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.buterfleoge.whale.biz.travel.TravelBiz;
+import com.buterfleoge.whale.type.protocol.travel.GetRouteRequest;
+import com.buterfleoge.whale.type.protocol.travel.GetRouteResponse;
+
+/**
+ * 
+ * 路线发团相关处理
+ * 
+ * @author Brent24
+ *
+ */
+
+@Controller
+@RequestMapping("/travel")
+public class TravelController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(TravelController.class);
+
+	@Autowired
+	private TravelBiz travelBiz;
+
+	@ResponseBody
+	@RequestMapping(value = "/route", method = RequestMethod.GET)
+	public GetRouteResponse getRoute(GetRouteRequest request) throws Exception {
+		GetRouteResponse response = new GetRouteResponse();
+		travelBiz.getRoute(request, response);
+		return response;
+	}
+}
