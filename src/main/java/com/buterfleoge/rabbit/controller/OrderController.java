21dddb5746f0dc67ddd86db0c64789250d11ca74
dongjunpeng <dongjunpeng0918@gmail.com>
2016-05-01 01:28:03 +0800

    5.1

--- src/main/java/com/buterfleoge/rabbit/controller/OrderController.java
+++ src/main/java/com/buterfleoge/rabbit/controller/OrderController.java
@@ -3,6 +3,16 @@
  */
 package com.buterfleoge.rabbit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.buterfleoge.whale.biz.order.OrderBiz;
+import com.buterfleoge.whale.type.protocol.order.GetCurrentOrderRequest;
+import com.buterfleoge.whale.type.protocol.order.GetCurrentOrderResponse;
+
 /**
  * 
  * 订单相关处理
@@ -10,6 +20,20 @@ package com.buterfleoge.rabbit.controller;
  * @author Brent24
  *
  */
+
+@Controller
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private OrderBiz orderBiz;
+
+    @ResponseBody
+    @RequestMapping(value = "/route", method = RequestMethod.GET)
+    public GetCurrentOrderResponse getRoute(GetCurrentOrderRequest request) throws Exception {
+        GetCurrentOrderResponse response = new GetCurrentOrderResponse();
+        orderBiz.getCurrentOrder(request, response);
+        return response;
+    }
 
 }
