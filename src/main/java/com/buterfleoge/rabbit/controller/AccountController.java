743ba3289d048da1da93ac7c9e5e5ed553fa0fee
zhenzong <zhenzong.peng@icloud.com>
2016-05-12 18:31:49 +0800

    account

--- src/main/java/com/buterfleoge/rabbit/controller/AccountController.java
+++ src/main/java/com/buterfleoge/rabbit/controller/AccountController.java
@@ -3,8 +3,6 @@ package com.buterfleoge.rabbit.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,25 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.buterfleoge.whale.Constants.SessionKey;
 import com.buterfleoge.whale.biz.account.AccountBiz;
+import com.buterfleoge.whale.dao.AccountInfoRepository;
 import com.buterfleoge.whale.dao.AccountSettingRepository;
-import com.buterfleoge.whale.type.enums.AccountType;
 import com.buterfleoge.whale.type.protocol.Response;
 import com.buterfleoge.whale.type.protocol.account.DeleteContactsRequest;
-import com.buterfleoge.whale.type.protocol.account.EmailExistRequest;
 import com.buterfleoge.whale.type.protocol.account.GetBasicInfoRequest;
 import com.buterfleoge.whale.type.protocol.account.GetBasicInfoResponse;
 import com.buterfleoge.whale.type.protocol.account.GetContactsRequest;
 import com.buterfleoge.whale.type.protocol.account.GetContactsResponse;
-import com.buterfleoge.whale.type.protocol.account.LoginRequest;
 import com.buterfleoge.whale.type.protocol.account.PostBasicInfoRequest;
 import com.buterfleoge.whale.type.protocol.account.PostContactsRequest;
 import com.buterfleoge.whale.type.protocol.account.PostContactsResponse;
-import com.buterfleoge.whale.type.protocol.account.PutContactsRequest;
-import com.buterfleoge.whale.type.protocol.account.RegisterRequest;
-import com.buterfleoge.whale.type.protocol.account.RegisterResponse;
-import com.buterfleoge.whale.type.protocol.account.ValidateEmailRequest;
-import com.buterfleoge.whale.type.protocol.order.GetOrdersRequest;
-import com.buterfleoge.whale.type.protocol.order.GetOrdersResponse;
+import com.buterfleoge.whale.type.protocol.account.object.AccountBasicInfo;
 
 /**
  * 账户相关处理
@@ -43,63 +34,40 @@ import com.buterfleoge.whale.type.protocol.order.GetOrdersResponse;
 @RequestMapping("/account")
 public class AccountController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
-
     @Autowired
     private AccountBiz accountBiz;
 
     @Autowired
-    private AccountSettingRepository accountSettingRepository;
+    private AccountInfoRepository accountInfoRepository;
 
     @Autowired
-    private HttpServletRequest httpRequest;
-
-    @ResponseBody
-    @RequestMapping(value = "/email", method = RequestMethod.POST)
-    public Response checkEmailExist(EmailExistRequest request) throws Exception {
-        Response response = new Response();
-        accountBiz.isEmailExist(request, response);
-        return response;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public RegisterResponse register(RegisterRequest request) throws Exception {
-
-        RegisterResponse response = new RegisterResponse();
-        request.setType(AccountType.USER);
-        accountBiz.registerByEmail(request, response);
-        if (response.hasError()) {
-            return response;
-        }
-        response.getAccountInfo().setPassword(null);
-        HttpSession session = httpRequest.getSession(true);
-        session.setAttribute(SessionKey.ACCOUNT_BASIC_INFO, response.getAccountInfo());
-        return response;
-    }
+    private AccountSettingRepository accountSettingRepository;
 
     @ResponseBody
     @RequestMapping(value = "/basicinfo", method = RequestMethod.GET)
-    public GetBasicInfoResponse getBasicInfo(GetBasicInfoRequest request) throws Exception {
+    public GetBasicInfoResponse getBasicInfo(GetBasicInfoRequest request, HttpServletRequest httpServletRequest)
+            throws Exception {
         GetBasicInfoResponse response = new GetBasicInfoResponse();
-        accountBiz.getBasicInfo(request, response);
+        HttpSession session = httpServletRequest.getSession(false);
+        if (session == null) {
+            response.setAccountInfo(accountInfoRepository.findOne(2L));
+            response.setAccountSetting(accountSettingRepository.findOne(2L));
+            return response;
+        }
+        AccountBasicInfo basicInfo = (AccountBasicInfo) session.getAttribute(SessionKey.ACCOUNT_BASIC_INFO);
+        if (basicInfo == null) {
+            return response;
+        }
+        response.setAccountInfo(basicInfo.getAccountInfo());
+        response.setAccountSetting(basicInfo.getAccountSetting());
         return response;
     }
 
     @ResponseBody
     @RequestMapping(value = "/basicinfo", method = RequestMethod.POST)
     public Response updateBasicInfo(PostBasicInfoRequest request) throws Exception {
-
         Response response = new Response();
         accountBiz.updateBasicInfo(request, response);
-        return response;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/validateEmail", method = RequestMethod.GET)
-    public Response validateEmail(ValidateEmailRequest request) throws Exception {
-        Response response = new Response();
-        accountBiz.validateEmail(request, response);
         return response;
     }
 
@@ -120,14 +88,6 @@ public class AccountController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/contacts", method = RequestMethod.PUT)
-    public Response putContacts(PutContactsRequest request) throws Exception {
-        Response response = new Response();
-        accountBiz.putContacts(request, response);
-        return response;
-    }
-
-    @ResponseBody
     @RequestMapping(value = "/contacts", method = RequestMethod.DELETE)
     public Response deleteContacts(DeleteContactsRequest request) throws Exception {
         Response response = new Response();
@@ -135,39 +95,9 @@ public class AccountController {
         return response;
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/orders", method = RequestMethod.GET)
-    public GetOrdersResponse getOrders(GetOrdersRequest request) {
-
-        return null;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/orders", method = RequestMethod.POST)
-    public GetOrdersResponse postOrders(GetOrdersRequest request) {
-
-        return null;
-    }
-
-    // email登陆
-    @ResponseBody
-    @RequestMapping("/login")
-    public Response login(LoginRequest request) {
-        // Response<AccountInfo> response = new Response<AccountInfo>();
-        // try {
-        // request.getFirstDataItem().setType(AccountType.USER);
-        // accountBiz.loginByEmail(request, response);
-        // } catch (Exception e) {
-        // LOG.error("Login failed", e);
-        // response.setStatus(Status.SYSTEM_ERROR);
-        // }
-        // return response;
-        return null;
-    }
-
     @RequestMapping(value = "/**", method = RequestMethod.GET)
-    public String getOrderPage() {
-        return "order";
+    public String getAccountPage() {
+        return "account";
     }
 
 }
