cb474d07a8f8d35a6bf060890b69e7ee030c801a
zhenzong <zhenzong.peng@icloud.com>
2016-05-25 00:07:43 +0800

    重构代码

--- src/main/java/com/buterfleoge/rabbit/controller/OrderController.java
+++ src/main/java/com/buterfleoge/rabbit/controller/OrderController.java
@@ -2,24 +2,24 @@ package com.buterfleoge.rabbit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.buterfleoge.whale.biz.order.CodeGenerator;
 import com.buterfleoge.whale.biz.order.OrderBiz;
+import com.buterfleoge.whale.dao.OrderInfoRepository;
+import com.buterfleoge.whale.type.entity.OrderInfo;
 import com.buterfleoge.whale.type.protocol.Response;
-import com.buterfleoge.whale.type.protocol.account.object.AccountBasicInfo;
 import com.buterfleoge.whale.type.protocol.order.CancelOrderRequest;
 import com.buterfleoge.whale.type.protocol.order.CreateOrderRequest;
-import com.buterfleoge.whale.type.protocol.order.GenerateCodeRequest;
-import com.buterfleoge.whale.type.protocol.order.GetBriefRequest;
-import com.buterfleoge.whale.type.protocol.order.GetBriefResponse;
+import com.buterfleoge.whale.type.protocol.order.CreateOrderResponse;
+import com.buterfleoge.whale.type.protocol.order.GetBriefOrdersRequest;
+import com.buterfleoge.whale.type.protocol.order.GetBriefOrdersResponse;
 import com.buterfleoge.whale.type.protocol.order.GetDiscountRequest;
 import com.buterfleoge.whale.type.protocol.order.GetDiscountResponse;
-import com.buterfleoge.whale.type.protocol.order.GetOrderDetailRequest;
-import com.buterfleoge.whale.type.protocol.order.GetOrderDetailResponse;
+import com.buterfleoge.whale.type.protocol.order.GetOrderRequest;
+import com.buterfleoge.whale.type.protocol.order.GetOrderResponse;
 import com.buterfleoge.whale.type.protocol.order.NewOrderRequest;
 import com.buterfleoge.whale.type.protocol.order.NewOrderResponse;
 import com.buterfleoge.whale.type.protocol.order.RefoundRequest;
@@ -34,7 +34,6 @@ import com.buterfleoge.whale.type.protocol.order.ValidateCodeResponse;
  * @author Brent24
  *
  */
-
 @Controller
 @RequestMapping("/order")
 public class OrderController extends RabbitController {
@@ -43,30 +42,28 @@ public class OrderController extends RabbitController {
     private OrderBiz orderBiz;
 
     @Autowired
-    private CodeGenerator codeGenerator;
+    private OrderInfoRepository orderInfoRepository;
 
     @ResponseBody
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public NewOrderResponse newOrder(NewOrderRequest request) throws Exception {
-        AccountBasicInfo accountBasicInfo = getAccountBasicInfo();
-        Long accountid = accountBasicInfo.getAccountInfo().getAccountid();
         NewOrderResponse response = new NewOrderResponse();
-        orderBiz.newOrder(accountid, request, response);
-        return response;
-    }
-
-    @RequestMapping(value = "/detail", method = RequestMethod.GET)
-    public GetOrderDetailResponse getOrderDetail(GetOrderDetailRequest request) throws Exception {
-        GetOrderDetailResponse response = new GetOrderDetailResponse();
-        orderBiz.getOrderDetail(request, response);
+        orderBiz.newOrder(requireAccountid(), request, response);
         return response;
     }
 
     @ResponseBody
     @RequestMapping(value = "/order", method = RequestMethod.POST)
-    public Response createOrder(@ModelAttribute("request") CreateOrderRequest request) throws Exception {
-        Response response = new Response();
-        orderBiz.createOrder(request, response);
+    public Response createOrder(CreateOrderRequest request) throws Exception {
+        CreateOrderResponse response = new CreateOrderResponse();
+        orderBiz.createOrder(requireAccountid(), request, response);
+        return response;
+    }
+
+    @RequestMapping(value = "/order", method = RequestMethod.GET)
+    public GetOrderResponse getOrder(GetOrderRequest request) throws Exception {
+        GetOrderResponse response = new GetOrderResponse();
+        orderBiz.getOrder(requireAccountid(), request, response);
         return response;
     }
 
@@ -74,31 +71,15 @@ public class OrderController extends RabbitController {
     @RequestMapping(value = "/order", method = RequestMethod.DELETE)
     public Response cancelOrder(CancelOrderRequest request) throws Exception {
         Response response = new Response();
-        orderBiz.cancelOrder(request, response);
+        orderBiz.cancelOrder(requireAccountid(), request, response);
         return response;
     }
 
     @ResponseBody
     @RequestMapping(value = "/brief", method = RequestMethod.GET)
-    public GetBriefResponse getRoute(GetBriefRequest request) throws Exception {
-        GetBriefResponse response = new GetBriefResponse();
-        orderBiz.getBriefOrders(request, response);
-        return response;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/discount", method = RequestMethod.GET)
-    public GetDiscountResponse getDiscount(GetDiscountRequest request) throws Exception {
-        GetDiscountResponse response = new GetDiscountResponse();
-        orderBiz.getDiscount(request, response);
-        return response;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/discountcode", method = RequestMethod.POST)
-    public ValidateCodeResponse validateDiscountCode(ValidateCodeRequest request) throws Exception {
-        ValidateCodeResponse response = new ValidateCodeResponse();
-        orderBiz.validateDiscountCode(request, response);
+    public GetBriefOrdersResponse getRoute(GetBriefOrdersRequest request) throws Exception {
+        GetBriefOrdersResponse response = new GetBriefOrdersResponse();
+        orderBiz.getBriefOrders(requireAccountid(), request, response);
         return response;
     }
 
@@ -106,22 +87,31 @@ public class OrderController extends RabbitController {
     @RequestMapping(value = "/refound", method = RequestMethod.GET)
     public RefoundResponse getRefoundInfo(RefoundRequest request) throws Exception {
         RefoundResponse response = new RefoundResponse();
-        orderBiz.getRefoundInfo(request, response);
+        orderBiz.getRefoundInfo(requireAccountid(), request, response);
         return response;
     }
 
-    @RequestMapping(value = "/**", method = RequestMethod.GET)
-    public String getOrderPage() throws Exception {
-        return "order";
-    }
-
-    // 下面都是测试用的
     @ResponseBody
-    @RequestMapping(value = "/generate/code", method = RequestMethod.POST)
-    public Response generateCode(GenerateCodeRequest request) throws Exception {
-        Response response = new Response();
-        codeGenerator.generate(request.getCount(), request.getValue(), request.getStartTime(), request.getEndTime());
+    @RequestMapping(value = "/discount", method = RequestMethod.GET)
+    public GetDiscountResponse getDiscount(GetDiscountRequest request) throws Exception {
+        GetDiscountResponse response = new GetDiscountResponse();
+        orderBiz.getDiscount(requireAccountid(), request, response);
         return response;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/discountcode", method = RequestMethod.GET)
+    public ValidateCodeResponse validateDiscountCode(ValidateCodeRequest request) throws Exception {
+        ValidateCodeResponse response = new ValidateCodeResponse();
+        orderBiz.validateDiscountCode(requireAccountid(), request, response);
+        return response;
+    }
+
+    @RequestMapping(value = "/{orderid}", method = RequestMethod.GET)
+    public String getOrderPage(@PathVariable Long orderid) throws Exception {
+        Long accountid = requireAccountid();
+        OrderInfo orderInfo = orderInfoRepository.findByOrderidAndAccountid(orderid, accountid);
+        return orderInfo == null ? "redirect:notfound" : "order";
     }
 
 }
