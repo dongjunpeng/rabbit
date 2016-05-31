575a5782a9d97d0dec3b1b47386372dc2180dfca
zhenzong <zhenzong.peng@icloud.com>
2016-05-31 23:06:45 +0800

    order

--- src/main/java/com/buterfleoge/rabbit/RabbitJacksonAnnotationIntrospector.java
+++ src/main/java/com/buterfleoge/rabbit/RabbitJacksonAnnotationIntrospector.java
@@ -2,21 +2,29 @@ package com.buterfleoge.rabbit;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.Parser;
 import org.springframework.format.Printer;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.datetime.DateTimeFormatAnnotationFormatterFactory;
 import org.springframework.format.number.NumberFormatAnnotationFormatterFactory;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import com.buterfleoge.whale.type.formatter.ImagePathAnnotationFormatterFactory;
 import com.buterfleoge.whale.type.formatter.ImagePathFormat;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.introspect.Annotated;
@@ -28,6 +36,8 @@ import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
  *
  */
 public class RabbitJacksonAnnotationIntrospector extends JacksonAnnotationIntrospector {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RabbitJacksonAnnotationIntrospector.class);
 
     /**
      * serial version uid
@@ -70,6 +80,25 @@ public class RabbitJacksonAnnotationIntrospector extends JacksonAnnotationIntros
         return super.findSerializer(a);
     }
 
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object findDeserializer(Annotated a) {
+        DateTimeFormat dateTimeFormat = _findAnnotation(a, DateTimeFormat.class);
+        if (dateTimeFormat != null) {
+            DateTimeFormatAnnotationFormatterFactory factory = new DateTimeFormatAnnotationFormatterFactory();
+            Parser<Date> parser = (Parser<Date>) factory.getParser(dateTimeFormat, Date.class);
+            return new RabbitDerializer<Date>(parser);
+        }
+
+        NumberFormat numberFormat = _findAnnotation(a, NumberFormat.class);
+        if (numberFormat != null) {
+            NumberFormatAnnotationFormatterFactory factory = new NumberFormatAnnotationFormatterFactory();
+            Parser<Number> parser = (Parser<Number>) factory.getParser(numberFormat, Number.class);
+            return new RabbitDerializer<Number>(parser);
+        }
+        return super.findDeserializer(a);
+    }
+
     private class RabbitSerializer<T> extends JsonSerializer<T> {
 
         private Printer<T> printer;
@@ -109,6 +138,26 @@ public class RabbitJacksonAnnotationIntrospector extends JacksonAnnotationIntros
                 gen.writeString(printer.print(t, locale));
             }
             gen.writeEndArray();
+        }
+    }
+
+    private class RabbitDerializer<T> extends JsonDeserializer<T> {
+
+        private Parser<T> parser;
+
+        public RabbitDerializer(Parser<T> parser) {
+            this.parser = parser;
+        }
+
+        @Override
+        public T deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            String token = p.getText();
+            try {
+                return StringUtils.hasText(token) ? parser.parse(token, ctxt.getLocale()) : null;
+            } catch (ParseException e) {
+                LOG.error("parse failed", e);
+                return null;
+            }
         }
 
     }
